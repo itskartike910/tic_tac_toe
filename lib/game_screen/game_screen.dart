e78b759a1ba9d01b48d04a5335d46db4856ba08e
Kartik <kumarkartik147359@gmail.com>
2024-01-02 22:10:14 +0530

    complete working app

---
+++ lib/game_screen/game_screen.dart
@@ -0,0 +1,294 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class GameScreen extends StatefulWidget {
+  String p1, p2;
+  GameScreen({super.key, required this.p1, required this.p2});
+
+  @override
+  State<GameScreen> createState() => GameScreenState();
+}
+
+class GameScreenState extends State<GameScreen> {
+  String player1 = "", player2 = "";
+
+  bool oTurn = true;
+  int xWins = 0, oWins = 0;
+  int filledBoxes = 0;
+  String winner = "";
+  String result = "...";
+
+  List<String> displayXO = ["", "", "", "", "", "", "", "", ""];
+  List<int> winningIndices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    player1 = widget.p1;
+    player2 = widget.p2;
+    if (player1.isEmpty) {
+      player1 = "Player 1";
+    }
+    if (player2.isEmpty) {
+      player2 = "Player 2";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Tic Tac Toe',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.amberAccent,
+        elevation: 6,
+        shadowColor: const Color.fromARGB(255, 249, 23, 23),
+        titleSpacing: 20,
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: const Color.fromARGB(255, 178, 242, 255),
+        child: Center(
+          child: Column(children: [
+            const Expanded(
+              flex: 0,
+              child: Padding(
+                padding: EdgeInsets.only(top: 5, bottom: 2),
+                child: Text(
+                  "Score Board",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                      color: Color.fromARGB(255, 2, 2, 151)),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                  alignment: Alignment.center,
+                  width: 360,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 12),
+                            child: Text(
+                              player1,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                  color: Color.fromARGB(255, 2, 2, 151)),
+                            ),
+                          ),
+                          Text(
+                            oWins.toString(),
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                                color: Color.fromARGB(255, 2, 2, 151)),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 12),
+                            child: Text(
+                              player2,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                  color: Color.fromARGB(255, 2, 2, 151)),
+                            ),
+                          ),
+                          Text(
+                            xWins.toString(),
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                                color: Color.fromARGB(255, 2, 2, 151)),
+                          ),
+                        ],
+                      )
+                    ],
+                  )),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: GridView.builder(
+                    itemCount: 9,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3),
+                    itemBuilder: (BuildContext cotext, int index) {
+                      return GestureDetector(
+                          onTap: () {
+                            tapped(index);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(3.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color:  winningIndices.contains(index) ? Colors.pinkAccent : Colors.amberAccent,
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                  width: 4,
+                                  color: const Color.fromARGB(255, 86, 64, 255),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  displayXO[index],
+                                  style: const TextStyle(fontSize: 80),
+                                ),
+                              ),
+                            ),
+                          ));
+                    }),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Text(
+                result,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                  color: Color.fromARGB(255, 166, 0, 0),
+                  overflow: TextOverflow.fade,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20, top: 6),
+              child: ElevatedButton(
+                onPressed: () {
+                  reset();
+                },
+                style: const ButtonStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll<Color>(Colors.purpleAccent),
+                  fixedSize: MaterialStatePropertyAll<Size>(Size(150, 40)),
+                ),
+                child: const Text(
+                  "Reset Game",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  void tapped(int index) {
+    setState(() {
+      filledBoxes++;
+      if (oTurn && displayXO[index] == "") {
+        displayXO[index] = "⭕";
+        oTurn = !oTurn;
+      } else if (!oTurn && displayXO[index] == "") {
+        displayXO[index] = "❌";
+        oTurn = !oTurn;
+      }
+      checkWinner();
+    });
+  }
+
+  void checkWinner() {
+    setState(() {
+      if (displayXO[0] == displayXO[1] &&
+          displayXO[0] == displayXO[2] &&
+          displayXO[0] != "") {
+        winner = displayXO[0];
+        winningIndices.addAll([0, 1, 2]);
+      } else if (displayXO[3] == displayXO[4] &&
+          displayXO[3] == displayXO[5] &&
+          displayXO[3] != "") {
+        winner = displayXO[3];
+        winningIndices.addAll([3, 4, 5]);
+      } else if (displayXO[6] == displayXO[7] &&
+          displayXO[6] == displayXO[8] &&
+          displayXO[6] != "") {
+        winner = displayXO[6];
+        winningIndices.addAll([6, 7, 8]);
+      } else if (displayXO[0] == displayXO[3] &&
+          displayXO[0] == displayXO[6] &&
+          displayXO[0] != "") {
+        winner = displayXO[0];
+        winningIndices.addAll([0, 3, 6]);
+      } else if (displayXO[1] == displayXO[7] &&
+          displayXO[1] == displayXO[4] &&
+          displayXO[1] != "") {
+        winner = displayXO[1];
+        winningIndices.addAll([1, 4, 7]);
+      } else if (displayXO[2] == displayXO[5] &&
+          displayXO[2] == displayXO[8] &&
+          displayXO[2] != "") {
+        winner = displayXO[2];
+        winningIndices.addAll([2, 5, 8]);
+      } else if (displayXO[0] == displayXO[4] &&
+          displayXO[0] == displayXO[8] &&
+          displayXO[0] != "") {
+        winner = displayXO[0];
+        winningIndices.addAll([0, 4, 8]);
+      } else if (displayXO[2] == displayXO[4] &&
+          displayXO[2] == displayXO[6] &&
+          displayXO[2] != "") {
+        winner = displayXO[2];
+        winningIndices.addAll([2, 4, 6]);
+      }
+    });
+    if (winner == "⭕" && result == "...") {
+      setState(() {
+        oWins++;
+        result = "$player1 wins (⭕).";
+      });
+    } else if (winner == "❌" && result == "...") {
+      setState(() {
+        xWins++;
+        result = "$player2 wins (❌).";
+      });
+    }
+
+    if (filledBoxes == 9 && result == "...") {
+      setState(() {
+        result = "There is a Tie.";
+      });
+    }
+  }
+
+  void reset() {
+    setState(() {
+      winner = "";
+      result = "...";
+      displayXO = ["", "", "", "", "", "", "", "", ""];
+      oTurn = true;
+      filledBoxes = 0;
+      winningIndices = [];
+    });
+  }
+}
---
+++ lib/player_screen/player_screen.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/game_screen/game_screen.dart';
+
+class PlayerScreen extends StatelessWidget {
+  PlayerScreen({super.key});
+
+  final player1 = TextEditingController();
+  final player2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Tic Tac Toe',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.amberAccent,
+        elevation: 6,
+        shadowColor: const Color.fromARGB(255, 249, 23, 23),
+        titleSpacing: 20,
+      ),
+      body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: const Color.fromARGB(255, 178, 242, 255),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 120,
+                width: 360,
+                alignment: Alignment.topCenter,
+                child: Image.asset(
+                  "assets/TicTacToe.png",
+                  fit: BoxFit.fill,
+                ),
+              ),
+              const SizedBox(
+                // width: 360,
+                height: 140,
+                child: Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text(
+                    "About:\nTic Tac Toe is a two-player game played on a 3x3 grid. Players take turns marking a square with their symbol (either ⭕ or ❌). The first player to get three of their symbols in a row (vertically, horizontally, or diagonally) wins the game.",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 147, 0, 0),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              ),
+              const Text(
+                "Enter Player 1 Name (⭕)",
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: TextField(
+                  controller: player1,
+                  decoration: const InputDecoration(
+                    labelText: "Player 1 Name",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.games_outlined,
+                      color: Colors.pinkAccent,
+                    ),
+                  ),
+                ),
+              ),
+              const Text(
+                "Enter Player 2 Name (❌)",
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: TextField(
+                  controller: player2,
+                  decoration: const InputDecoration(
+                    labelText: "Player 2 Name",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.games_outlined,
+                      color: Colors.pinkAccent,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 12),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => GameScreen(
+                                  p1: player1.text,
+                                  p2: player2.text,
+                                ))).then((result) {});
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.purpleAccent),
+                    fixedSize: MaterialStatePropertyAll<Size>(Size(150, 40)),
+                  ),
+                  child: const Text(
+                    'Start Game',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
