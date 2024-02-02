1ca5091b29a94fe8b6cf95af8b91ce5eb5c8b64f
Kartik <kumarkartik147359@gmail.com>
2024-02-02 18:48:11 +0530

    improved

--- lib/game_screen/game_screen.dart
+++ lib/game_screen/game_screen.dart
@@ -73,95 +73,99 @@ class GameScreenState extends State<GameScreen> {
             Expanded(
               flex: 1,
               child: Container(
-                  alignment: Alignment.center,
-                  width: 360,
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 12),
-                            child: Text(
-                              player1,
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 24,
-                                  color: Color.fromARGB(255, 2, 2, 151)),
-                            ),
-                          ),
-                          Text(
-                            oWins.toString(),
+                alignment: Alignment.center,
+                width: 360,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 12),
+                          child: Text(
+                            player1,
+                            textAlign: TextAlign.center,
                             style: const TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 24,
                                 color: Color.fromARGB(255, 2, 2, 151)),
                           ),
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 12),
-                            child: Text(
-                              player2,
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 24,
-                                  color: Color.fromARGB(255, 2, 2, 151)),
-                            ),
-                          ),
-                          Text(
-                            xWins.toString(),
+                        ),
+                        Text(
+                          oWins.toString(),
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                              color: Color.fromARGB(255, 2, 2, 151)),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 12),
+                          child: Text(
+                            player2,
+                            textAlign: TextAlign.center,
                             style: const TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 24,
                                 color: Color.fromARGB(255, 2, 2, 151)),
                           ),
-                        ],
-                      )
-                    ],
-                  )),
+                        ),
+                        Text(
+                          xWins.toString(),
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                              color: Color.fromARGB(255, 2, 2, 151)),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ),
             Expanded(
               flex: 3,
               child: Padding(
                 padding: const EdgeInsets.all(15),
                 child: GridView.builder(
-                    itemCount: 9,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3),
-                    itemBuilder: (BuildContext cotext, int index) {
-                      return GestureDetector(
-                          onTap: () {
-                            tapped(index);
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(3.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color:  winningIndices.contains(index) ? Colors.pinkAccent : Colors.amberAccent,
-                                borderRadius: BorderRadius.circular(20),
-                                border: Border.all(
-                                  width: 4,
-                                  color: const Color.fromARGB(255, 86, 64, 255),
-                                ),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  displayXO[index],
-                                  style: const TextStyle(fontSize: 80),
-                                ),
-                              ),
+                  itemCount: 9,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (BuildContext cotext, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        tapped(index);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: winningIndices.contains(index)
+                                ? Colors.pinkAccent
+                                : Colors.amberAccent,
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              width: 4,
+                              color: const Color.fromARGB(255, 86, 64, 255),
                             ),
-                          ));
-                    }),
+                          ),
+                          child: Center(
+                            child: Text(
+                              displayXO[index],
+                              style: const TextStyle(fontSize: 80),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
             Expanded(
@@ -224,42 +228,58 @@ class GameScreenState extends State<GameScreen> {
           displayXO[0] == displayXO[2] &&
           displayXO[0] != "") {
         winner = displayXO[0];
-        winningIndices.addAll([0, 1, 2]);
+        if (winningIndices.isEmpty) {
+          winningIndices.addAll([0, 1, 2]);
+        }
       } else if (displayXO[3] == displayXO[4] &&
           displayXO[3] == displayXO[5] &&
           displayXO[3] != "") {
         winner = displayXO[3];
-        winningIndices.addAll([3, 4, 5]);
+        if (winningIndices.isEmpty) {
+          winningIndices.addAll([3, 4, 5]);
+        }
       } else if (displayXO[6] == displayXO[7] &&
           displayXO[6] == displayXO[8] &&
           displayXO[6] != "") {
         winner = displayXO[6];
-        winningIndices.addAll([6, 7, 8]);
+        if (winningIndices.isEmpty) {
+          winningIndices.addAll([6, 7, 8]);
+        }
       } else if (displayXO[0] == displayXO[3] &&
           displayXO[0] == displayXO[6] &&
           displayXO[0] != "") {
         winner = displayXO[0];
-        winningIndices.addAll([0, 3, 6]);
+        if (winningIndices.isEmpty) {
+          winningIndices.addAll([0, 3, 6]);
+        }
       } else if (displayXO[1] == displayXO[7] &&
           displayXO[1] == displayXO[4] &&
           displayXO[1] != "") {
         winner = displayXO[1];
-        winningIndices.addAll([1, 4, 7]);
+        if (winningIndices.isEmpty) {
+          winningIndices.addAll([1, 4, 7]);
+        }
       } else if (displayXO[2] == displayXO[5] &&
           displayXO[2] == displayXO[8] &&
           displayXO[2] != "") {
         winner = displayXO[2];
-        winningIndices.addAll([2, 5, 8]);
+        if (winningIndices.isEmpty) {
+          winningIndices.addAll([2, 5, 8]);
+        }
       } else if (displayXO[0] == displayXO[4] &&
           displayXO[0] == displayXO[8] &&
           displayXO[0] != "") {
         winner = displayXO[0];
-        winningIndices.addAll([0, 4, 8]);
+        if (winningIndices.isEmpty) {
+          winningIndices.addAll([0, 4, 8]);
+        }
       } else if (displayXO[2] == displayXO[4] &&
           displayXO[2] == displayXO[6] &&
           displayXO[2] != "") {
         winner = displayXO[2];
-        winningIndices.addAll([2, 4, 6]);
+        if (winningIndices.isEmpty) {
+          winningIndices.addAll([2, 4, 6]);
+        }
       }
     });
     if (winner == "⭕" && result == "...") {
@@ -272,9 +292,7 @@ class GameScreenState extends State<GameScreen> {
         xWins++;
         result = "$player2 wins (❌).";
       });
-    }
-
-    if (filledBoxes == 9 && result == "...") {
+    } else if (filledBoxes == 9 && result == "...") {
       setState(() {
         result = "There is a Tie.";
       });
